b207557ffd061bef1fdb2fe23a4e6c98e820264e
pelacorbandit <144488606+pelacorbandit@users.noreply.github.com>
2023-12-08 16:55:51 -0500

    Delete datasources.tf

--- datasources.tf
+++
@@ -1,9 +0,0 @@
-data "intersight_network_element_summary" "fi-a" {
-  count = var.assigned_switch_a ? 1 : 0
-  serial = var.fi_serial_number_a
-}
-
-data "intersight_network_element_summary" "fi-b" {
-  count = var.assigned_switch_b ? 1 : 0
-  serial = var.fi_serial_number_b
-}
